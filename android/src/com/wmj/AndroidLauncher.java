648270eaac1fb330eedc40bb6a9fe60f307c6936
marek <mjedrzejewski92@gmail.com>
2016-03-02 14:41:29 +0100

    Initial commit

---
+++ android/src/com/wmj/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wmj;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wmj.ZdzichuRuns;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZdzichuRuns(), config);
+	}
+}
