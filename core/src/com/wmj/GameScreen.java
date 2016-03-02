0e42a23527ff2f7368190c39a55a93803a2154b4
vostanminor <lukasz.wsol@gmail.com>
2016-03-02 21:47:50 +0100

    create architecture

---
+++ core/src/com/wmj/GameScreen.java
@@ -0,0 +1,58 @@
+package com.wmj;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by vostan on 02.03.16.
+ */
+public class GameScreen implements Screen {
+
+    private GameStage stage;
+
+    public GameScreen() {
+        stage = new GameStage();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        //Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Update the stage
+        stage.draw();
+        stage.act(delta);
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- desktop/src/com/wmj/desktop/DesktopLauncher.java
+++ desktop/src/com/wmj/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package com.wmj.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wmj.Constants;
 import com.wmj.ZdzichuRuns;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.APP_WIDTH;
+		config.height = Constants.APP_HEIGHT;
 		new LwjglApplication(new ZdzichuRuns(), config);
 	}
 }
